94699d90f77ff487a124dc8cb109736040f0793b
Sam Rich <srich@srich-a01.vmware.com>
2019-05-29 14:21:06 -0400

    adding the initial project files to repo

---
+++ On The Map/On The Map/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  On The Map
+//
+//  Created by Sam Rich on 5/29/19.
+//  Copyright © 2019 Sam Rich. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
