f4ff962b2c3c0fa2d3651ef29c037286aa6d98e6
Sam Rich <srich@srich-a01.vmware.com>
2019-06-16 08:38:24 -0400

    Updated login VC with better constraints, text field returns to work better across device sizes

--- On The Map/On The Map/Controller/LoginVC.swift
+++ On The Map/On The Map/Controller/LoginVC.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var emailAddressTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -19,8 +19,20 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailAddressTextField.delegate = self
+        passwordTextField.delegate = self
         emailAddressTextField.text = ""
         passwordTextField.text = ""
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        //unsubscribeFromKeyboardNotifications()
     }
     
     //    MARK: Button Handlers
@@ -56,4 +68,49 @@ class LoginVC: UIViewController {
         show(alertVC, sender: nil)
     }
     
+    // Advance the keyboard in the case of smaller devices
+    // TODO: This doest seem to be quite working as I thought...check on this again
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailAddressTextField {
+            emailAddressTextField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        } else {
+            passwordTextField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    
+    // MARK: Keyboard Methods from my MemeMe project
+    // Behavior for keyboard hiding -> reset the view to its base level
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    //Behavior for keyboard showing: move the frame up vertically per the height of the keyboard
+    @objc func keyboardWillShow(_ notification:Notification) {
+        // TODO: I'm getting some odd jumping behavior on this animation, but it could be the toolbar constraints?
+        if (emailAddressTextField.isEditing){
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    //Determine the keyboard height
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    //Add keyboard notifications
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    //Remove keyboard notifications
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 }
