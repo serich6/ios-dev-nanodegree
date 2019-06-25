f23f8f9058150ad1cff44687ea5fe5cd5c0dd5af
Sam Rich <srich@srich-a01.vmware.com>
2019-06-25 10:10:01 -0400

    added nav bar to map

---
+++ On The Map/On The Map/Controller/MapNavVC.swift
@@ -0,0 +1,27 @@
+//
+//  MapNavVC.swift
+//  On The Map
+//
+//  Created by Sam Rich on 6/25/19.
+//  Copyright © 2019 Sam Rich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapNavVC: UIViewController, UINavigationControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+}
