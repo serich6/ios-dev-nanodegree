03b78b832ce5e8b4f1e5780367813d4915c91bc2
Sam Rich <srich@srich-a01.vmware.com>
2019-07-21 17:02:25 -0400

    Question list displaying

--- Trivia/Trivia/Controller/QuestionListVC.swift
+++ Trivia/Trivia/Controller/QuestionListVC.swift
@@ -10,15 +10,19 @@ import Foundation
 import UIKit
 import CoreData
 
-class QuestionListVC: UIViewController {
+class QuestionListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var questionTextLabel: UITextView!
     @IBOutlet weak var correctAnswerLabel: UILabel!
     @IBOutlet weak var yourAnswerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     var dataController: DataController!
     var category: LocalCategory!
     var questionsToDisplay: [LocalQuestion]!
     
+    
     override func viewDidLoad() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         questionTextLabel.text = "Select a question from the list below for review."
         correctAnswerLabel.isHidden = true
         yourAnswerLabel.isHidden = true
@@ -32,9 +36,28 @@ class QuestionListVC: UIViewController {
         do {
             let fetchedQuestions = try dataController.viewContext.fetch(fetchRequest as! NSFetchRequest<NSFetchRequestResult>) as! [LocalQuestion]
             questionsToDisplay = fetchedQuestions
-            print(questionsToDisplay)
         } catch {
             fatalError("Failed to fetch category: \(error)")
         }
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionsToDisplay.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell")!
+        let question = self.questionsToDisplay[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = question.text
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let question = self.questionsToDisplay[(indexPath as NSIndexPath).row]
+        print("selected a quetsion")
     }
 }
