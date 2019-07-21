9714c6d8b1268074abc4b4f7994864a6763293c4
Sam Rich <srich@srich-a01.vmware.com>
2019-07-20 22:03:46 -0400

    Added stats draft page

---
+++ Trivia/Trivia/Controller/StatsMainVC.swift
@@ -0,0 +1,20 @@
+//
+//  StatsMainVC.swift
+//  Trivia
+//
+//  Created by Sam Rich on 7/20/19.
+//  Copyright © 2019 Sam Rich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StatsMainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "My Stats"
+    }
+    
+    
+}
