044b9efa31ba3aa92b24c0f684ecc49fac706e59
Sam Rich <srich@srich-a01.vmware.com>
2019-07-21 14:27:30 -0400

    Added basic label updates and pulling categories for stats, need to fix table display

--- Trivia/Trivia/Controller/StatsMainVC.swift
+++ Trivia/Trivia/Controller/StatsMainVC.swift
@@ -8,18 +8,61 @@
 
 import Foundation
 import UIKit
+import CoreData
 
-class StatsMainVC: UIViewController, UITableViewDelegate {
+class StatsMainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var bestCategoryLabel: UILabel!
     @IBOutlet weak var worseCategoryLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     var dataController: DataController!
+    var categoriesToDisplay: [LocalCategory]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "My Stats"
-        //tableView.delegate = self
+        getCategories()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        setBestCategory()
+        setWorseCategory()
     }
     
+    func getCategories() {
+        let fetchRequest:NSFetchRequest<LocalCategory> = LocalCategory.fetchRequest()
+        do {
+            let fetchedCategories = try dataController.viewContext.fetch(fetchRequest as! NSFetchRequest<NSFetchRequestResult>) as! [LocalCategory]
+            categoriesToDisplay = fetchedCategories
+            print("got categories: \(categoriesToDisplay)")
+        } catch {
+            fatalError("Failed to fetch category: \(error)")
+        }
+    }
     
+    func setBestCategory() {
+        bestCategoryLabel.text = categoriesToDisplay.first?.name
+    }
+    
+    func setWorseCategory() {
+        worseCategoryLabel.text = categoriesToDisplay.last?.name
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoriesToDisplay.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("created cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryStatsCell")!
+        let category = self.categoriesToDisplay[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = "\(category.name)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("row tapped")
+    }
 }
